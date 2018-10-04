f02c0fc29388d2dfe1c2aff0287237715e59e4cf
Max <max.francis2000@gmail.com>
2018-10-04 12:57:37 +0000

    Added error message expectation to spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -12,11 +12,18 @@ describe DockingStation do
     expect(bike.working?).to be true
   end
   describe '#release_bike' do
-    it 'releases a bike' do
-      bike = Bike.new
-      subject.dock(bike)
-      # we want to release the bike we docked
-      expect(subject.release_bike).to eq bike
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
     end
   end
+  
+  describe '#dock' do
+    it 'raises an error when full' do
+      subject.dock(Bike.new)
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    end
+  end
+  
+  
+  
 end  
